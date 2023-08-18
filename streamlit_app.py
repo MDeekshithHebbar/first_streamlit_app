66a578682af899b802ca0e74a6fdef3406dd9013
M Deekshith Hebbar <103358004+MDeekshithHebbar@users.noreply.github.com>
2023-08-18 09:42:01 +0530

    Update streamlit_app.py
    adddee

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title('my parents are new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('dosa,idli')
-streamlit.text('puri & sagu')
-streamlit.text('chapathi')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
